6c4fab4f41ceb52031668369d8294b477eb80d82
scitech257 <115054015+scitech257@users.noreply.github.com>
2022-10-04 22:02:56 -0400

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,19 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     MotorDriver.MotorRun(Motor.A, Dir.forward, 16)
     MotorDriver.MotorRun(Motor.B, Dir.forward, 16)
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
     MotorDriver.MotorRun(Motor.A, Dir.backward, 16)
     MotorDriver.MotorRun(Motor.B, Dir.backward, 16)
 })
-input.onGesture(Gesture.Shake, function () {
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
     MotorDriver.MotorRun(Motor.A, Dir.forward, 16)
     MotorDriver.MotorRun(Motor.B, Dir.backward, 16)
 })
 pins.setPull(DigitalPin.P0, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
-basic.forever(function () {
+basic.forever(function on_forever() {
     if (pins.digitalReadPin(DigitalPin.P0) == 0) {
         MotorDriver.MotorRun(Motor.A, Dir.backward, 10)
         MotorDriver.MotorRun(Motor.B, Dir.forward, 10)
@@ -30,4 +30,5 @@ basic.forever(function () {
         MotorDriver.MotorRun(Motor.A, Dir.forward, 10)
         MotorDriver.MotorRun(Motor.B, Dir.forward, 10)
     }
+    
 })
